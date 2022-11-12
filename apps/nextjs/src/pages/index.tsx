a1abf6c1adf7a299e2fd342d47117164366d3cfa
Joseph Scholl <composerofsoftware@gmail.com>
2022-11-11 20:13:55 -0700

    formatted and sort the table

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -22,17 +22,19 @@ const EventRow: React.FC<{
   selected: boolean;
   selectEvent: (eventId: string) => void;
 }> = ({ event, selected, selectEvent }) => {
+  let date = event.date.toUTCString();
+  date = date.substring(0, date.length - 13);
   return (
     <tr
-      className={`flex-1 table-row  ${!selected && "hover:bg-slate-200"} ${selected && "bg-slate-300 hover:bg-slate-400"}`}
+      className={`table-row border  border-separate ${!selected && "hover:bg-slate-200"} ${selected && "bg-slate-300 hover:bg-slate-400"}`}
       onClick={(e) => {
         e.preventDefault();
         selectEvent(event.id);
       }}
     >
-      <td className="">{event.title}</td>
-      <td className="">{event.date.toUTCString()}</td>
-      <td className="flex-1">{event.description}</td>
+      <td className="border px-2">{date}</td>
+      <td className="border px-2">{event.title}</td>
+      <td className="border px-2">{event.description}</td>
     </tr>
   );
 }
@@ -66,51 +68,54 @@ const Home: NextPage = () => {
         <meta name="description" content="what happened??" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        <div className="flex m-4">
-          <h1 className="font-bold text-2xl mr-auto">Edit and add interesting events in church history</h1>
-          <AuthShowcase />
+      <main className="flex flex-col p-4">
+        <div className="flex-1 bg-white">
+
+          <div className="flex">
+            <h1 className="font-bold text-2xl mr-auto">Edit and add interesting events in church history</h1>
+            <AuthShowcase />
+          </div>
+
+          <form className="flex gap-2 flex-col"
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (!date) return;
+              addEvent.mutate({ date: new Date(date,), description, tags: [], title });
+              setDescription('');
+              setTitle('');
+              setDate('');
+            }}
+          >
+            <div className="flex">
+              <label className="w-12">Date</label>
+              <input value={date} onChange={(event) => setDate(event.target.value)}
+                className="border border-b-2" type="date" />
+            </div>
+            <div className="flex">
+              <label className="w-12">Title</label>
+              <input value={title} onChange={(event) => setTitle(event.target.value)} id="titleField" className="flex-1 border border-b-2" type="text" />
+            </div>
+            <div className="flex flex-col">
+              <label>Description</label>
+              <textarea value={description} onChange={(event) => setDescription(event.target.value)} className="flex-1 border border-b-2 m-1 " />
+            </div>
+            <button type="submit"
+              className="my-4 w-20 px-2 p-1 border bg-green-400">Save</button>
+          </form>
         </div>
 
-        <form className="m-4 flex gap-2 flex-col"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (!date) return;
-            addEvent.mutate({ date: new Date(date,), description, tags: [], title });
-            setDescription('');
-            setTitle('');
-            setDate('');
-          }}
-        >
-          <div className="flex">
-            <label className="w-12">Date</label>
-            <input value={date} onChange={(event) => setDate(event.target.value)}
-              className="border border-b-2" type="date" />
-          </div>
-          <div className="flex">
-            <label className="w-12">Title</label>
-            <input value={title} onChange={(event) => setTitle(event.target.value)} id="titleField" className="flex-1 border border-b-2" type="text" />
-          </div>
-          <div className="flex flex-col">
-            <label>Description</label>
-            <textarea value={description} onChange={(event) => setDescription(event.target.value)} className="flex-1 border border-b-2 m-1 " />
-          </div>
-          <button type="submit"
-            className="w-20 border bg-green-400">Save</button>
-        </form>
 
-
-        {/* <div className="m-4"> */}
-        <table className="m-4 border border-slate-500">
-          <thead>
-            <tr>
-              <th className="text-start">Title</th>
-              <th className="text-start">Date</th>
-              <th className="text-start">Description</th>
+        <table className="w-full ">
+          <thead className="">
+            <tr className=" border border-slate-500 bg-white">
+              <th className="sticky top-0 pl-2 border text-start">Date</th>
+              <th className="sticky top-0 pl-2 border text-start">Title</th>
+              <th className="sticky top-0 pl-2 border text-start">Description</th>
             </tr>
           </thead>
 
-          <tbody>
+
+          <tbody className="border border-1 border-collapse border-gray h-[20vh] overflow-y-scroll">
             {eventsQuery.data?.map((event) => {
               return (
                 <EventRow key={event.id} event={event} selected={event.id === selectedEvent}
@@ -124,8 +129,9 @@ const Home: NextPage = () => {
             })}
           </tbody>
         </table>
-        {/* </div> */}
-        {selectedEvent && <div className="m-4 flex gap-4">
+
+
+        {selectedEvent && <div className="py-4 w-full bg-white flex gap-4 sticky bottom-0">
           <button className="w-20 bg-yellow-200 border">Edit (TODO)</button>
           <button className="w-20 bg-red-400" onClick={() => {
             deleteEvent.mutate(selectedEvent);
--- packages/api/src/router/event.ts
+++ packages/api/src/router/event.ts
@@ -1,28 +1,39 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
-import { z } from "zod"
+import { z } from "zod";
 
 export const eventRouter = router({
-    all: publicProcedure.query(( { ctx }) => {
-        return ctx.prisma.interestingEvent.findMany();
-    }),
-    create: publicProcedure
-    .input(z.object({ date: z.date(), title: z.string(), description: z.string(), tags: z.array(z.string()) }))
+  all: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.interestingEvent.findMany({
+      orderBy: [{ month: "asc" }, { day: "asc" }],
+    });
+  }),
+  create: publicProcedure
+    .input(
+      z.object({
+        date: z.date(),
+        title: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-        console.log('herew++++++++++++++++++');
-        console.log(input.tags);
-        const r = await ctx.prisma.interestingEvent.create({ data: {
-            date: input.date, 
-            month: input.date.getUTCMonth() + 1,
-            day: input.date.getUTCDate(),
-            title: input.title,
-            description: input.description,
-        } });
-        console.log(r);
-        return r;
+      console.log("herew++++++++++++++++++");
+      console.log(input.tags);
+      const r = await ctx.prisma.interestingEvent.create({
+        data: {
+          date: input.date,
+          month: input.date.getUTCMonth() + 1,
+          day: input.date.getUTCDate(),
+          title: input.title,
+          description: input.description,
+        },
+      });
+      console.log(r);
+      return r;
     }),
-    deleteById: publicProcedure
+  deleteById: publicProcedure
     .input(z.string())
     .mutation(async ({ ctx, input }) => {
-        return ctx.prisma.interestingEvent.delete({ where: { id: input } })
+      return ctx.prisma.interestingEvent.delete({ where: { id: input } });
     }),
 });
